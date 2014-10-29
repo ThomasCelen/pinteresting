a5668f21718f300dda5ff5acefb8e72ee54222e9
Thomas Celen <th.celen@gmail.com>
2014-10-29 15:54:39 +0100

    user/pin validation added

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,8 @@
 class PinsController < ApplicationController
   respond_to :html, :xml, :json
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -12,7 +14,7 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
     respond_with(@pin)
   end
 
@@ -20,7 +22,7 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     @pin.save
     respond_with(@pin)
   end
@@ -35,11 +37,18 @@ class PinsController < ApplicationController
     respond_with(@pin)
   end
 
+# Use callbacks to share common setup or constraints between actions.
   private
     def set_pin
       @pin = Pin.find(params[:id])
     end
 
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, alert: "You are not authorized to edit this pin." if @pin.nil?
+    end
+
+# Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
     end
